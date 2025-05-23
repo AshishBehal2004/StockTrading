dca4295db66cc10786641848d2973b4c022047c6
behay002 <behay002@mymail.unisa.edu.au>
2025-05-23 12:10:48 +0930

    First Commit
    Started Step One
    So far completed the constructor part, and completeing the add method()

--- src/unisa/dse/a2/students/DSEList.java
+++ src/unisa/dse/a2/students/DSEList.java
@@ -10,66 +10,112 @@ public class DSEList implements List {
 	
 	public Node head;
 	private Node tail;
-
-	public DSEList() {
-		
+	int size = 0;
+	
+	
+	public DSEList() 
+	{
+		this.head = null;
+		this.tail = null;
+		this.size = 0;
 	}
-	public DSEList(Node head_) {
+	
+	public DSEList(Node head_) 
+	{
+		this.head = head_;
+		this.tail = head_;
+		this.size= 1;
+		
+		head_.next = null;
+		head_.prev = null;	
 	}
 	
 	//Takes a list then adds each element into a new list
-	public DSEList(DSEList other) { // Copy constructor. 
+	public DSEList(DSEList other) 
+	{ 
+		Node current = other.head;
+		 
+		while(current != null)
+		{
+			String current_data = current.getString();
+			
+			this.add(current_data);
+			current = current.next;
+			
+		}
+		
 	}
 
 	//remove the String at the parameter's index
-	public String remove(int index) {
+	public String remove(int index) 
+	{
 
 	}
 
 	//returns the index of the String parameter 
-	public int indexOf(String obj) {
+	public int indexOf(String obj) 
+	{
+		
 	}
 	
 	//returns String at parameter's index
-	public String get(int index) {
+	public String get(int index) 
+	{
+		
 	}
 
 	//checks if there is a list
-	public boolean isEmpty() {
+	public boolean isEmpty() 
+	{
+		
 	}
 
 	//return the size of the list
-	public int size() {
+	public int size() 
+	{
+		
 	}
 	
 	//Take each element of the list a writes them to a string 
 	@Override
-	public String toString() {
+	public String toString() 
+	{
+		
 	}
 
 	//add the parameter String at of the end of the list
-	public boolean add(String obj) {
+	public boolean add(String obj) 
+	{
+		Node newNode = new Node(null, tail, obj);
 	}
 
 	//add String at parameter's index
-	public boolean add(int index, String obj) {
+	public boolean add(int index, String obj) 
+	{
+		
 	}
 
 	//searches list for parameter's String return true if found
-	public boolean contains(String obj) {
+	public boolean contains(String obj) 
+	{
+		
 	}
 
 	//removes the parameter's String form the list
-	public boolean remove(String obj) {
+	public boolean remove(String obj) 
+	{
+		
 	}
 	
 	@Override
-	public int hashCode() {
+	public int hashCode() 
+	{
 		return 0;
 	}
 
 	@Override
-	public boolean equals(Object other) {
+	public boolean equals(Object other)
+	{
 		return true;
 	}
 	
